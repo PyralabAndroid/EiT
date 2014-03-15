23ce30eeb6af87c98da9b40bc59d409bd64c780b
Paweł Rąbel <prabel91@gmail.com>
2014-03-15 22:24:30 +0100

    Added baseScheduleFragment

---
+++ eit/src/main/java/pl/eit/androideit/eit/schedule_fragment/BaseScheduleFragment.java
@@ -0,0 +1,23 @@
+package pl.eit.androideit.eit.schedule_fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockFragment;
+
+public class BaseScheduleFragment extends RoboSherlockFragment {
+
+    public BaseScheduleFragment() {
+        super();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
