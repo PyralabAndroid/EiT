f6a7e920394ba813495020ab4ac44600570c203a
Paweł Rąbel <prabel91@gmail.com>
2014-04-07 23:44:04 +0200

    k

--- eit/src/main/java/pl/eit/androideit/eit/ogloszenia/JsonFields.java
+++ eit/src/main/java/pl/eit/androideit/eit/ogloszenia/JsonFields.java
@@ -1,29 +1,19 @@
 package pl.eit.androideit.eit.ogloszenia;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-/**
- * Created by Robert on 2014-04-01.
- */
-public class JsonFields
-{
+public class JsonFields {
 
-        public String text;
-        public String title;
-        public String images_url;
-        public ArrayList<Appendix> appendix;
+    public String text;
+    public String title;
+    public String images_url;
+    public ArrayList<Appendix> appendix;
 
-    public static class Appendix
-    {
-       public String link;
-       public String href;
+    public static class Appendix {
+        public String link;
+        public String href;
 
     }
-
-
-
-
 }
 
 
